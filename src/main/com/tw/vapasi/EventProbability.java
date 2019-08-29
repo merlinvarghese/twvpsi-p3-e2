4bc3b1d257d95d16052285f02359277a177e3181
“Srividya <srividyakittu@gmail.com>
2019-08-29 15:21:19 +0530

    [VidyaAlva,Srividya]Refactor. Equals Test Cases

--- src/main/com/tw/vapasi/EventProbability.java
+++ src/main/com/tw/vapasi/EventProbability.java
@@ -1,18 +1,17 @@
 package com.tw.vapasi;
 
+//Compare the occurrence of 2 independent events
 class EventProbability {
     private double occurrence;
 
     EventProbability(double occurrence) {
         this.occurrence = occurrence;
     }
-
+@Override
     public boolean equals(Object event) {
-        if (!(event instanceof EventProbability))
-            return false;
-        return (this.occurrence == ((EventProbability)event).occurrence);
+       if ((event==null) || (event.getClass()!=this.getClass()))
+        return false;
+
+        return this.occurrence == ((EventProbability)event).occurrence;
     }
-
-
-
 }
--- src/test/com/tw/vapasi/EventProbabilityTest.java
+++ src/test/com/tw/vapasi/EventProbabilityTest.java
@@ -3,19 +3,41 @@ package com.tw.vapasi;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
+@SuppressWarnings("ALL")
 class EventProbabilityTest {
 
     @Test
-    void expectFalseWhenHeadForCoinWithSpadeForCards() {
+    void expectFalseWhenIsHeadForCoinWithSpadeForCards() {
         EventProbability coinHeadEvent = new EventProbability(0.5);
         EventProbability cardSpadeEvent = new EventProbability(0.25);
-        Assertions.assertNotEquals(coinHeadEvent, cardSpadeEvent);
+        assertFalse(coinHeadEvent.equals(cardSpadeEvent));
     }
     @Test
-    void expectTrueWhenHeadForCoinWithSpadeAndHeartsForCards() {
+    void expectTrueWhenIsHeadForCoinWithSpadeAndHeartsForCards() {
         EventProbability coinHeadEvent = new EventProbability(0.5);
         EventProbability cardSpadeEvent = new EventProbability(0.5);
-        Assertions.assertEquals(coinHeadEvent, cardSpadeEvent);
+        assertTrue(coinHeadEvent.equals(cardSpadeEvent));
+    }
+    @Test
+    void expectFalseWhenArgumentEventProbabilityisNull() {
+        EventProbability coinHeadEvent = null;
+        EventProbability cardSpadeHeartEvent = new EventProbability(0.5);
+        assertFalse(cardSpadeHeartEvent.equals(coinHeadEvent));
+    }
+    @Test
+    void expectFalseWhenArgumentIsNotEventProbability() {
+        String stringObject = new String("check");
+        EventProbability cardSpadeHeartEvent = new EventProbability(0.5);
+        assertFalse(cardSpadeHeartEvent.equals(stringObject));
+    }
+    @Test
+    void expectTrueWhenArgumentIsEventProbabilityContentsMatch() {
+        EventProbability coinHeadEvent = new EventProbability(0.5);
+        EventProbability cardSpadeHeartEvent = new EventProbability(0.5);
+        assertTrue(cardSpadeHeartEvent.equals(coinHeadEvent));
     }
 }
